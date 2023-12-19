eab05ead49ca043debc94e25924825db67abc16a
zainy123 <zainulabden018@gmail.com>
2023-12-19 12:19:32 +0500

    first commit

---
+++ lib/database/index.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDB = async () => {
+  if (cached.conn) return cached.conn;
+  if (!MONGODB_URI) throw new Error(`Cannot connect to Mongo's database`);
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
